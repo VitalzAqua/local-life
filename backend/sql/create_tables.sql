b84359ee1854a1c1b80c48b75f1c89b6c3427e4a
Victor Wang <74549506+VitalzAqua@users.noreply.github.com>
2025-06-20 09:48:16 -0400

    Delete backend directory

--- backend/sql/create_tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE stores (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  category TEXT NOT NULL,
-  location GEOGRAPHY(Point, 4326) NOT NULL,
-  attributes JSONB
-);
-CREATE INDEX idx_stores_location ON stores USING GIST(location);
-CREATE INDEX idx_stores_category ON stores(category);
